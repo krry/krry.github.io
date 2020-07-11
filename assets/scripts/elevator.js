9ee63b163aeb92d1c5ead1e4384ab0eb5c296b3f
Kerry Snyder <krry@me.com>
2020-07-11 01:48:24 -0700

    adjust scroll behavior to suit elevator

--- assets/scripts/elevator.js
+++ assets/scripts/elevator.js
@@ -1,8 +1,20 @@
 document.addEventListener(("DOMContentLoaded"), function() {
   var elEl = document.querySelector('.elevator-button');
+  var eb = document.getElementById('eb');
+
+  var scrollSwitchThenElevator = function () {
+    document.documentElement.style.scrollBehavior = "auto";
+    eb.click();
+    setTimeout(function() {
+      document.documentElement.style.scrollBehavior = "smooth";
+    }, 15000)
+  }
+
   var elevator = new Elevator({
-    element: elEl,
+    element: eb,
     mainAudio: '/assets/files/elevator-music.m4a',
     endAudio: '/assets/files/Gong-sound.mp3',
   });
+
+  elEl.addEventListener('click', scrollSwitchThenElevator)
 })
