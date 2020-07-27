1668334fb410ee8d9d6a2b95017367b0e95b1aa1
Kerry Snyder <krry@me.com>
2020-07-26 18:55:06 -0700

    add fallback background image and framerate floor

--- assets/scripts/waves.js
+++ assets/scripts/waves.js
@@ -1,13 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    VANTA.WAVES({
-        el: "#waves",
-        color: 0x280664,
-        shininess: 32.00,
-        waveHeight: 12.00,
-        waveSpeed: 1.50,
-        mouseControls: false,
-        touchControls: false,
-        zoom: 1
-    })
-    document.getElementById("waves").classList.add("on");
-})
+document.addEventListener("DOMContentLoaded", () => {
+  var start = null;
+  var last = null;
+  window.requestAnimationFrame(fpsMeasureLoop);
+  function fpsMeasureLoop(timestamp) {
+    if (start == null) {
+      last = start = timestamp;
+      return;
+    }
+    var dTime = timestamp - last;
+    if (dTime > 33) {
+      // If more than 33ms since last frame (i.e. below 30fps)
+      document.getElementsByTagName("body")[0].className = "paused";
+    }
+    window.requestAnimationFrame(fpsMeasureLoop);
+  }
+
+  VANTA.WAVES({
+    el: "#waves",
+    color: 0x280664,
+    shininess: 32.0,
+    waveHeight: 12.0,
+    waveSpeed: 1.5,
+    mouseControls: false,
+    touchControls: false,
+    zoom: 1,
+  });
+  document.getElementById("waves").classList.add("on");
+});
