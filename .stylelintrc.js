e4d7d458d7ce394f4f6574e91ebd533a992859da
Kerry Snyder <kerry@atmanaut.me>
2021-04-26 17:08:42 -0700

    add recent projects let stylelint fuck things sideways

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
-    "extends": [
-        "stylelint-config-sass-guidelines",
-        "stylelint-config-prettier"
-    ],
-    "rules": {
-        "max-nesting-depth": 2,
-        "number-leading-zero": "always",
-        "indentation": 2
-    },
-    "exclude": [
-        "./assets/main.scss"
-    ]
+	extends: [
+		'stylelint-config-sass-guidelines',
+		'stylelint-config-prettier',
+		'stylelint-config-standard',
+	],
+	rules: {
+		indentation: 'tab',
+		'max-nesting-depth': 2,
+		'number-leading-zero': 'always',
+		'at-rule-no-unknown': null,
+		'scss/at-rule-no-unknown': null,
+		'declaration-block-trailing-semicolon': null,
+		'no-descending-specificity': null,
+		'declaration-empty-line-before': null,
+		'value-list-comma-newline-after': null,
+		'declaration-colon-newline-after': null,
+	},
+	exclude: ['./assets/main.scss'],
 }
